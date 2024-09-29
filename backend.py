ab24a4c10b8765d45596b651d34f4b7b1549e31f
tostako <juan.arteaga.f@uniautonoma.edu.co>
2024-09-28 21:14:30 -0500

    conexion base de datos fronted agregar

--- backend.py
+++ backend.py
@@ -1,5 +1,5 @@
 import mysql.connector
-import datetime
+from datetime import datetime
 from flask import Flask, request, jsonify
 
 # Configuración de la aplicación Flask
@@ -17,12 +17,8 @@ cursor = bd.cursor()
 # Ruta para agregar frutas
 @app.route('/agregar_fruta', methods=['POST'])
 def agregar_fruta_endpoint():
-    datos = request.json  # Obtener los datos JSON de la solicitud
-    if datos:
-        result = agregar_fruta(datos)  # Procesar los datos
-        return jsonify({"message": "Producto recibido correctamente", "status": "success"})
-    else:
-        return jsonify({"message": "No se recibieron datos", "status": "error"}), 400
+    datos = request.json
+    agregar_fruta(datos)
 
 # Función para agregar fruta a la base de datos
 def agregar_fruta(datos):
@@ -30,13 +26,36 @@ def agregar_fruta(datos):
     nombre = str(datos.get('nombre'))
     precio = int(datos.get('precio'))
     origen = str(datos.get('origen'))
-    cantidad = int(datos.get('cantidad')) # Asumiendo que `cantidad` también debe ser insertado
-    cursor.execute(
-        "INSERT INTO productos (id_producto, nombre, precio, origen) VALUES (%s, %s, %s, %s)",
-        (id_producto, nombre, precio, origen)
+    cantidad = int(datos.get('cantidad'))
+    fecha_hoy = datetime.now().now().date()
+    cursor.execute("SELECT id_producto FROM productos WHERE id_producto = %s", (id_producto,))
+    producto_existente = cursor.fetchone()
+    if producto_existente:
+        cursor.execute("SELECT cantidad_actual FROM inventario WHERE id_producto = %s", (id_producto,))
+        cantidad_movimiento = cursor.fetchone()
+        if cantidad_movimiento:
+            cantidad_movimiento = cantidad_movimiento[0] + cantidad
+            cursor.execute(
+                "INSERT INTO inventario(id_producto, cantidad_actual, fecha_movimiento, cantidad_movimiento) VALUES (%s, %s, %s, %s)",
+                (id_producto, cantidad_movimiento, fecha_hoy, cantidad))
+        else:
+            cursor.execute(
+                    "INSERT INTO inventario(id_producto, cantidad_actual, fecha_movimiento, cantidad_movimiento) VALUES (%s, %s, %s, %s)",
+                    (id_producto, cantidad, fecha_hoy, cantidad))
+        
+    else:
+        cursor.execute(
+            "INSERT INTO productos (id_producto, nombre, precio, origen) VALUES (%s, %s, %s, %s)",
+            (id_producto, nombre, precio, origen)
     )
+        cursor.execute(
+                "INSERT INTO inventario(id_producto, cantidad_actual, fecha_movimiento, cantidad_movimiento) VALUES (%s, %s, %s, %s)",
+                (id_producto, cantidad, fecha_hoy, cantidad))
+        
+        
+    
+
     bd.commit()
-    return "True"
 
 if __name__ == '__main__':
     app.run(debug=True)  # Inicia la aplicación Flask en modo de depuración
